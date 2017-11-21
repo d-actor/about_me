00fc977592f5e8c9acc2fdf18a13e9087c3c5635
Dan Actor <danltactor@gmail.com>
2017-11-20 21:03:31 -0700

    start add js effects

--- main.js
+++ main.js
@@ -6,3 +6,15 @@ $("a").hover(function(){
     }, function(){
     $(this).css("font-size", "16px");
 });
+
+// contact click animation
+
+$("#email").click( function(){
+  $("#email").animate({
+    width: "70%",
+    opacity: 0.4,
+    marginLeft: "0.6in",
+    fontSize: "3em",
+    borderWidth: "10px"
+  });
+});
